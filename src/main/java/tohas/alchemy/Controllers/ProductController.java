fe5f46dd161f5d40fdfc25ee94f7098a02867169
gshiporin <gshiporin@gmail.com>
2021-03-20 22:47:47 +0300

    Product entity

---
+++ src/main/java/tohas/alchemy/Controllers/ProductController.java
@@ -0,0 +1,34 @@
+package tohas.alchemy.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tohas.alchemy.Entities.Product;
+import tohas.alchemy.Repository.ProductRepository;
+
+import javax.validation.Valid;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(path = "/api/product/{id}", produces = "application/json")
+    public Object getProductById (@Valid @PathVariable Long id){
+        if (productRepository.existsById(id)) {
+            return productRepository.findById(id);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(path = "/api/product/new", consumes = "application/json")
+    public Object createNewProduct (@RequestBody Product product){
+        productRepository.save(product);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/tohas/alchemy/Entities/Product.java
+++ src/main/java/tohas/alchemy/Entities/Product.java
@@ -1,6 +1,7 @@
 package tohas.alchemy.Entities;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -12,20 +13,43 @@ public class Product {
     @Getter
     Long code;
 
+    @Getter
+    @Setter
     @OneToOne
     Substance substance;
 
+    @Getter
+    @Setter
     @OneToOne
     Vendor vendor_id;
 
+    @Getter
+    @Setter
     @OneToOne
     Qualification qualification_id;
 
+    @Getter
+    @Setter
     @OneToOne
     Packing packing_id;
 
+    @Getter
+    @Setter
     int quantity;
 
+    @Getter
+    @Setter
     long price;
 
+    public Product() {
+    }
+
+    public Product(Substance substance, Vendor vendor_id, Qualification qualification_id, Packing packing_id, int quantity, long price) {
+        this.substance = substance;
+        this.vendor_id = vendor_id;
+        this.qualification_id = qualification_id;
+        this.packing_id = packing_id;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
